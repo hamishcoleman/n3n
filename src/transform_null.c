104b88814b27f3bc0d9aec1aea558e620e556ee3
Hamish Coleman <hamish@zot.org>
2026-03-19 18:26:00 +1100

    Proposed speedup transop_decode_null
    Co-authored-by: Dmitry Ermakov <ermakovdmitry8@gmail.com>

--- src/transform_null.c
+++ src/transform_null.c
@@ -57,26 +57,49 @@ static int transop_encode_null (n2n_trans_op_t *arg,
 }
 
 
-static int transop_decode_null (n2n_trans_op_t *arg,
-                                uint8_t *outbuf,
-                                size_t out_len,
-                                const uint8_t *inbuf,
-                                size_t in_len,
-                                const uint8_t *peer_mac) {
+// static int transop_decode_null (n2n_trans_op_t *arg,
+//                                 uint8_t *outbuf,
+//                                 size_t out_len,
+//                                 const uint8_t *inbuf,
+//                                 size_t in_len,
+//                                 const uint8_t *peer_mac) {
 
-    int retval = -1;
+//     int retval = -1;
 
-    traceEvent(TRACE_DEBUG, "decode_null %lu", in_len);
-    if(out_len >= in_len) {
-        memcpy(outbuf, inbuf, in_len);
-        retval = in_len;
-    } else {
-        traceEvent(TRACE_DEBUG, "decode_null %lu too big for packet buffer", in_len);
+//     traceEvent(TRACE_DEBUG, "decode_null %lu", in_len);
+//     if(out_len >= in_len) {
+//         memcpy(outbuf, inbuf, in_len);
+//         retval = in_len;
+//     } else {
+//         traceEvent(TRACE_DEBUG, "decode_null %lu too big for packet buffer", in_len);
+//     }
+
+//     return retval;
+// }
+
+static inline int transop_decode_null(n2n_trans_op_t *arg,
+                                      uint8_t *outbuf,
+                                      size_t out_len,
+                                      const uint8_t *inbuf,
+                                      size_t in_len,
+                                      const uint8_t *peer_mac) {
+    (void)arg;          // unused
+    (void)peer_mac;     // unused
+
+    if (out_len < in_len) {
+#if TRACE_ENABLED && (TRACE_LEVEL >= TRACE_DEBUG)
+        traceEvent(TRACE_DEBUG, "decode_null %zu too big for packet buffer", in_len);
+#endif
+        return -1;
     }
 
-    return retval;
-}
+#if TRACE_ENABLED && (TRACE_LEVEL >= TRACE_DEBUG)
+    traceEvent(TRACE_DEBUG, "decode_null %zu", in_len);
+#endif
 
+    memcpy(outbuf, inbuf, in_len);
+    return (int)in_len;
+}
 
 int n2n_transop_null_init (const n2n_edge_conf_t *conf, n2n_trans_op_t *ttt) {
 
